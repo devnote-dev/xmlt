4832d2d8dca2db924216deef6f255bbc47274904
devnote-dev <inferno0745@gmail.com>
2022-04-06 03:23:10 +0100

    refactor(*): remove for native lib

--- src/xmlt.cr
+++ src/xmlt.cr
@@ -1,18 +1,4 @@
-require "./lexer.cr"
-require "./structs.cr"
-
 module XMLT
   VERSION = "0.1.0"
 end
 
-xml = <<-XML
-<?xml version="1.0" encoding="utf-8"?>
-<Person>
-  <firstname>foo</firstname>
-  <lastname>bar</lastname>
-  <!-- <gender>baz</male> -->
-</Person>
-XML
-
-tokens = XMLT::Lexer.new(xml).run
-pp tokens
