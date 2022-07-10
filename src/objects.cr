dfd2c1e88e7cae23b4f22366f269fb22ac72c4b9
devnote-dev <inferno0745@gmail.com>
2022-07-10 22:44:52 +0100

    feat: implement indent option for types

--- src/objects.cr
+++ src/objects.cr
@@ -1,9 +1,9 @@
 require "xml"
 
 struct Int
-  def to_xml(key : String? = nil) : String
+  def to_xml(*, key : String? = nil, indent = nil) : String
     if key
-      XML.build_fragment { |xml| xml.element(key) { xml.text to_s } }
+      XML.build_fragment(indent: indent) { |xml| xml.element(key) { xml.text to_s } }
     else
       to_s
     end
@@ -11,9 +11,9 @@ struct Int
 end
 
 struct Float
-  def to_xml(key : String? = nil) : String
+  def to_xml(*, key : String? = nil, indent = nil) : String
     if key
-      XML.build_fragment { |xml| xml.element(key) { xml.text to_s } }
+      XML.build_fragment(indent: indent) { |xml| xml.element(key) { xml.text to_s } }
     else
       to_s
     end
@@ -21,9 +21,9 @@ struct Float
 end
 
 class String
-  def to_xml(key : String? = nil) : String
+  def to_xml(*, key : String? = nil, indent = nil) : String
     if key
-      XML.build_fragment { |xml| xml.element(key) { xml.text to_s } }
+      XML.build_fragment(indent: indent) { |xml| xml.element(key) { xml.text to_s } }
     else
       to_s
     end
@@ -31,9 +31,9 @@ class String
 end
 
 struct Bool
-  def to_xml(key : String? = nil) : String
+  def to_xml(*, key : String? = nil, indent = nil) : String
     if key
-      XML.build_fragment { |xml| xml.element(key) { xml.text to_s } }
+      XML.build_fragment(indent: indent) { |xml| xml.element(key) { xml.text to_s } }
     else
       to_s
     end
@@ -41,9 +41,9 @@ struct Bool
 end
 
 struct Char
-  def to_xml(key : String? = nil) : String
+  def to_xml(*, key : String? = nil, indent = nil) : String
     if key
-      XML.build_fragment { |xml| xml.element(key) { xml.text to_s } }
+      XML.build_fragment(indent: indent) { |xml| xml.element(key) { xml.text to_s } }
     else
       to_s
     end
@@ -51,9 +51,9 @@ struct Char
 end
 
 struct Symbol
-  def to_xml(key : String? = nil) : String
+  def to_xml(*, key : String? = nil, indent = nil) : String
     if key
-      XML.build_fragment { |xml| xml.element(key) { xml.text to_s } }
+      XML.build_fragment(indent: indent) { |xml| xml.element(key) { xml.text to_s } }
     else
       to_s
     end
@@ -61,9 +61,9 @@ struct Symbol
 end
 
 struct Path
-  def to_xml(key : String? = nil) : String
+  def to_xml(*, key : String? = nil, indent = nil) : String
     if key
-      XML.build_fragment { |xml| xml.element(key) { xml.text @name } }
+      XML.build_fragment(indent: indent) { |xml| xml.element(key) { xml.text @name } }
     else
       @name
     end
@@ -71,74 +71,74 @@ struct Path
 end
 
 class Array
-  def to_xml(key : String = "item") : String
-    XML.build_fragment do |xml|
+  def to_xml(*, key : String = "item", indent = nil) : String
+    XML.build_fragment(indent: indent) do |xml|
       map(&.to_xml).each { |i| xml.element(key) { xml.text i } }
     end
   end
 end
 
 class Deque
-  def to_xml(key : String = "item") : String
-    XML.build_fragment do |xml|
+  def to_xml(*, key : String = "item", indent = nil) : String
+    XML.build_fragment(indent: indent) do |xml|
       map(&.to_xml).each { |i| xml.element(key) { xml.text i } }
     end
   end
 end
 
 struct Tuple
-  def to_xml(key : String = "item") : String
-    XML.build_fragment do |xml|
+  def to_xml(*, key : String = "item", indent = nil) : String
+    XML.build_fragment(indent: indent) do |xml|
       map(&.to_xml).each { |i| xml.element(key) { xml.text i } }
     end
   end
 end
 
 struct Set
-  def to_xml(key : String = "item") : String
-    XML.build_fragment do |xml|
+  def to_xml(*, key : String = "item", indent = nil) : String
+    XML.build_fragment(indent: indent) do |xml|
       map(&.to_xml).each { |i| xml.element(key) { xml.text i } }
     end
   end
 end
 
 class Hash
-  def to_xml : String
-    XML.build_fragment do |xml|
+  def to_xml(*, indent = nil) : String
+    XML.build_fragment(indent: indent) do |xml|
       each { |k, v| xml.element(k) { xml.text v.to_xml } }
     end
   end
 end
 
 struct NamedTuple
-  def to_xml : String
-    XML.build_fragment do |xml|
+  def to_xml(*, indent = nil) : String
+    XML.build_fragment(indent: indent) do |xml|
       each { |k, v| xml.element(k) { xml.text v.to_xml } }
     end
   end
 end
 
 struct Range
-  def to_xml(key : String = "item") : String
-    to_a.to_xml key
+    def to_xml(*, key : String = "item", indent = nil) : String
+    to_a.to_xml key: key, indent: indent
   end
 end
 
 struct Time
-  def to_xml(key : String? = nil) : String
+  def to_xml(*, key : String? = nil, indent = nil) : String
     fmt = Time::Format::RFC_3339.format self
     if key
-      XML.build_fragment { |xml| xml.element(key) { xml.text fmt } }
+      XML.build_fragment(indent: indent) { |xml| xml.element(key) { xml.text fmt } }
     else
       fmt
     end
   end
 
   struct Format
-    def to_xml(value : Time, key : String? = nil) : String
+    def to_xml(value : Time, *, key : String? = nil, indent = nil) : String
       fmt = format value
       if key
-        XML.build_fragment { |xml| xml.element(key) { xml.text fmt } }
+        XML.build_fragment(indent: indent) { |xml| xml.element(key) { xml.text fmt } }
       else
         fmt
       end
